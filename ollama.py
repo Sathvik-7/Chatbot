2c3779dccbbe69225601281af469059db5db97e6
Sathvik-7 <chilluvurisathvik@gmail.com>
2025-11-05 15:51:49 -0600

    Ollama-gemma3

---
+++ ollama.py
@@ -0,0 +1,31 @@
+import os 
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser   
+from langchain_community.llms import Ollama
+
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages([
+    ("system","You are a helpful assistant. Please response to the user queries"),
+    ("user","Question:{question}")
+])
+
+## streamlit framework
+st.title('Langchain Demo With Ollama API')
+input_text=st.text_input("Search the topic u want")
+
+# Ollama LLm 
+llm =Ollama(model="gemma3:1b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
